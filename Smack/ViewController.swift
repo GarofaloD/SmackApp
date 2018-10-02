83e3ae5ce6632c36366b2864b40ecdb9231f57be
Daniel Garofalo <danielgarofalo@Daniels-MacBook-Pro-2.local>
2018-10-02 16:24:36 -0500

    first commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Daniel Garofalo on 10/2/18.
+//  Copyright © 2018 Daniel Garofalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
